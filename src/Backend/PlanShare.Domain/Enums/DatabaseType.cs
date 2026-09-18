e28bc5ead61b868c7d9ab4efa98b7001fb49ecc9
Borges <borgespaulo72@yahoo.com.br>
2026-09-18 17:59:39 -0300

    fix: reescrevendo appsettings e launchSettings

---
+++ src/Backend/PlanShare.Domain/Enums/DatabaseType.cs
@@ -0,0 +1,8 @@
+﻿namespace PlanShare.Domain.Enums
+{
+    public enum DatabaseType
+    {
+        MySQL = 0,
+        SQLServer = 1
+    }
+}
--- src/Backend/PlanShare.Infrastructure/Extensions/ConfigurationExtensions.cs
+++ src/Backend/PlanShare.Infrastructure/Extensions/ConfigurationExtensions.cs
@@ -1,9 +1,34 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using Microsoft.EntityFrameworkCore.Storage;
+using Microsoft.Extensions.Configuration;
+using PlanShare.Domain.Enums;
 
 namespace PlanShare.Infrastructure.Extensions;
 public static class ConfigurationExtensions
 {
-    public static string ConnectionString(this IConfiguration configuration) => configuration.GetConnectionString("Connection")!;
+    public static string ConnectionString(this IConfiguration configuration)
+    {
+        var databaseType = configuration.GetDatabaseType();
+
+        if(databaseType == DatabaseType.MySQL)
+        {
+            return configuration.GetConnectionString("ConnectionMySQL")!;
+        }
+        else if(databaseType == DatabaseType.SQLServer)
+        {
+            return configuration.GetConnectionString("ConnectionSQLServer")!;
+        }
+        else
+        {
+            throw new NotSupportedException($"Database type '{databaseType}' is not supported.");
+        }
+    }
+
+    public static DatabaseType GetDatabaseType(this IConfiguration configuration)
+    {
+        var databaseType = configuration.GetConnectionString("DatabaseType")!;
+        
+        return Enum.Parse<DatabaseType>(databaseType);
+    }
 
     public static bool IsUnitTestEnviroment(this IConfiguration configuration)
     {
